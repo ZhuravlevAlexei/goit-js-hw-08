179f9ccc45e2c5044897aaa0bf8ca01921b6390b
ZhuravlevAlexei <110524860+ZhuravlevAlexei@users.noreply.github.com>
2022-12-28 15:46:36 +0200

    виправлення помилок за відео
    0:37 - виправлено - очищення об'єкту даних перед видаленням ключа
    1:38 1 - виправлено - слухач на інпут на форму та другий на submit також на форму

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,24 +10,26 @@ let StateData = {
   message: '',
 };
 
-feedbackForm.addEventListener('keyup', onFormInput);
-sbmButton.addEventListener('click', onButtonClick);
-
 function saveStorage() {
   localStorage.setItem(KEY_SUBMIT_STORAGE, JSON.stringify(StateData));
 }
 
 const throttledSaveStorage = throttle(saveStorage, 500);
 
-function onButtonClick(evt) {
+function onFormSubmit(evt) {
+  // console.log(evt);
   evt.preventDefault();
   inputEmail.value = '';
   inputMessage.value = '';
   console.log(StateData);
+  StateData.email = '';
+  StateData.message = '';
+  saveStorage();
   localStorage.removeItem(KEY_SUBMIT_STORAGE);
 }
 
 function onFormInput(evt) {
+  // console.log(evt);
   if (evt.target.name === 'email') {
     StateData.email = evt.target.value;
   }
@@ -36,6 +38,12 @@ function onFormInput(evt) {
   }
   throttledSaveStorage();
 }
+
+// feedbackForm.addEventListener('keyup', onFormInput);
+// sbmButton.addEventListener('click', onFormSubmit);
+
+feedbackForm.addEventListener('input', onFormInput);
+feedbackForm.addEventListener('submit', onFormSubmit);
 
 try {
   StateData = JSON.parse(localStorage.getItem(KEY_SUBMIT_STORAGE));
